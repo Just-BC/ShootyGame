596c6d6c981f619a2415738f1bc32101c0236fa1
domi-hamm64 <60869908+domi-hamm64@users.noreply.github.com>
2020-03-02 19:33:39 -0600

    Fix

---
+++ ShootyGame2/Assets/Scripts/BulletTrail.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTrail : MonoBehaviour
+{
+    [SerializeField] private float waitTime;
+    public IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(waitTime);
+        Destroy(gameObject);
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Timer());
+    }
+
+}
---
+++ ShootyGame2/Assets/Sprites/explode.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explode : MonoBehaviour
+{
+    public float radius;
+    public float strength;
+    // Start is called before the first frame update
+    public void Explode()
+    {
+        //GetComponent<MeshRenderer>().enabled = false;
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,radius);
+        foreach (Collider2D hit in colliders)
+        {
+            if(hit.GetComponent<Rigidbody2D>())
+            {
+                //hit.GetComponent<Rigidbody2D>().AddForceAtPosition(new Vector2(Mathf.Sin(Vector2.Angle(transform.eulerAngles, hit.transform.eulerAngles)*Mathf.Deg2Rad) * strength, Mathf.Cos(Vector2.Angle(transform.eulerAngles, hit.transform.eulerAngles) * Mathf.Deg2Rad) * strength), transform.position);
+                hit.GetComponent<Rigidbody2D>().AddForceAtPosition((-transform.position + hit.transform.position).normalized*strength, transform.position);
+                Destroy(gameObject);
+            }
+        }
+    }
+}
